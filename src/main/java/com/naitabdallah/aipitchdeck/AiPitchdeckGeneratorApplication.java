7ea97cd0a6f1cbf493a53bf022383278666db7ab
abdallah nait abdallah <naitabdallah.abdallah@gmail.com>
2025-11-06 14:16:53 +0100

    initialization spring backend

---
+++ src/main/java/com/naitabdallah/aipitchdeck/AiPitchdeckGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.naitabdallah.aipitchdeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPitchdeckGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPitchdeckGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naitabdallah/aipitchdeck/AiPitchdeckGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naitabdallah.aipitchdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPitchdeckGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
